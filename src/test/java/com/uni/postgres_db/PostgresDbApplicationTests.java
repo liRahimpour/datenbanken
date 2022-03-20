e75b13fb8f18d936526e0af6718f06053ae45679
Ali Rahimpour <li.rahimpour@gmail.com>
2022-03-20 14:36:17 +0100

    init

---
+++ src/test/java/com/uni/postgres_db/PostgresDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.postgres_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
